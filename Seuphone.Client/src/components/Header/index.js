2c0ac7f82d25f03d2a80876574a89e9072b1cd94
Emerson Melo Martins <emerson25xd@gmail.com>
2021-05-04 20:15:07 -0300

    Header collapse and footer adjustment

--- Seuphone.Client/src/components/Header/index.js
+++ Seuphone.Client/src/components/Header/index.js
@@ -5,31 +5,25 @@ import { FiSearch, FiShoppingCart } from "react-icons/fi";
 import { Cart, HeaderContainer } from "./styles";
 import logoImg from "../../assets/img/logo.png";
 import { useCart } from "../../hooks/useCart";
+import { Nav, Navbar } from "react-bootstrap";
 
 export function Header() {
-
   const { cart } = useCart();
   const cartSize = cart.length;
 
   return (
     <HeaderContainer>
-      <nav className="navbar navbar-dark bg-dark text-light seuphone-background">
+      <Navbar
+        className="navbar navbar-dark bg-dark text-light seuphone-background"
+        collapseOnSelect
+        expand={false}
+      >
         <div className="container">
-          <span className="navbar-brand" href="/">
-            <span>
-              <img src={logoImg} width="90px" alt="" />
-            </span>
+          <Navbar.Brand>
+            <img src={logoImg} width="90px" alt="" />
 
-            <a
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#togglerMenu"
-              href="/"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </a>
-          </span>
+            <Navbar.Toggle />
+          </Navbar.Brand>
 
           <form className="form-inline my-2 my-lg-0 mx-auto">
             <input
@@ -56,30 +50,28 @@ export function Header() {
           <Cart to="/cart" className="mx-3">
             <FiShoppingCart size={30} color="#FFF" />
             <div className="mx-2">
-              <span>
-                {cartSize}
-              </span>
+              <span>{cartSize}</span>
             </div>
           </Cart>
         </div>
 
         <div className="container">
-          <div className="collapse navbar-collapse" id="togglerMenu">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
+          <Navbar.Collapse>
+            <Nav className="mr-auto d-block">
+              <Nav.Item>
+                <Nav.Link eventKey="1" as={Link} to="/">
                   Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/products" className="nav-link">
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="2" as={Link} to="/products">
                   Produtos
-                </Link>
-              </li>
-            </ul>
-          </div>
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
         </div>
-      </nav>
+      </Navbar>
     </HeaderContainer>
   );
 }
